3ad3c51fad736163891231129d1faadf2e7b99cd
Scott Schuster <34114261+Windsor-schuster@users.noreply.github.com>
2023-06-08 12:31:07 -0700

    Add files via upload

---
+++ dt_frame.m
@@ -0,0 +1,23 @@
+function [plot] = dt_frame(filename, flipX,flipY, width, height,tstart,tend,spread)
+cd_data = load_cd_events(filename, flipX, flipY);
+
+x=cd_data.x;
+y=cd_data.y;
+ts=cd_data.ts;
+p=cd_data.p;
+CD=zeros(height,width);
+indexstart=find(ts==tstart, 1);
+indexend=find(ts==tend, 1, 'last' );
+
+for i=indexstart:indexend
+    xc=x(i);
+    yc=y(i);
+   CD(yc,xc)=CD(yc,xc) + p(i);
+end
+CD_1=CD/spread *255;
+%spread value tunes image precision through differentiating point values
+plot=image(CD_1);
+plot
+
+end
+
